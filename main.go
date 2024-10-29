e560423f2d1ab0122b1490e576222f41b5699cd1
oussamanmh <mo_nemamcha@esi.dz>
2024-10-29 16:39:13 +0100

    variables strings and numbers

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
-package main
+// Description: This file contains the code for the first part of the tutorial which is about variables in Go.
 
-import "fmt"
+package main // Package declaration which is the main package
 
+import "fmt" // Importing the fmt package which is used for printing to the console it stands for format
+
+// Main function
 func main() {
-    fmt.Println("Hello, World!")
+	//Strings
+	var nameOne string = "OUSSAMA"
+	var nameTwo = "YASSINE"
+	var nameThree string //Default value is ""
+
+	nameThree = "AMINE" //Assigning a value to nameThree
+
+	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameFour := "IMANE" //Short hand declaration
+
+	fmt.Println(nameFour)
+
+	//Integers
+	var ageOne int = 20 //Explicit declaration
+	var ageTwo = 30
+	ageThree := -40 //Short hand declaration
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	//Bits & Memory
+	var ageFour int8 = 127  // 8 bits for signed numbers range [-128, 127]
+	var ageFive uint8 = 255 // 8 bits for positive numbers only range [0, 255]
+	fmt.Println(ageFour, ageFive)
+
+	//Floats
+	var scoreOne float32 = 25.98 // 32 bits for floating point numbers
+	var scoreTwo float64 = 25.98 // 64 bits for floating point numbers
+	fmt.Println(scoreOne, scoreTwo)
 }
