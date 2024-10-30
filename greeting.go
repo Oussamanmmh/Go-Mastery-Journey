4004d81fd162d9b14be8c05ac08dc1ec042cde56
oussamanmh <mo_nemamcha@esi.dz>
2024-10-30 06:19:07 +0100

    package scope

---
+++ greeting.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func Greeting() {
+	fmt.Println("Hello, World!")
+}
+
+func displayScore() {
+	fmt.Println("Score is ", score)
+}
--- main.go
+++ main.go
@@ -12,9 +12,14 @@ func myFunction(n string) {
 func circleArea(r float64) float64 {
 	return math.Pi * r * r
 }
+
+var score int = 300
+
 func main() {
-	var name string = "oussama"
-	myFunction(name)
-	fmt.Println("the name now:", name)
-	fmt.Println("the area :", circleArea(4))
+	// var name string = "oussama"
+	// myFunction(name)
+	// fmt.Println("the name now:", name)
+	// fmt.Println("the area :", circleArea(4))
+	Greeting()
+	displayScore()
 }
